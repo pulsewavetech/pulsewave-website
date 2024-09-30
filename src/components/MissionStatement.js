687fbf960d65065ccb9105fb136e1ff5ac88f808
Zach Riser <zachary.riser@gmail.com>
2024-09-30 19:32:54 -0400

    added favicon

--- src/components/MissionStatement.js
+++ src/components/MissionStatement.js
@@ -6,7 +6,7 @@ const MissionStatement = () => {
       <div className="container">
         <h2>Our Mission</h2>
         <p>
-          PulseWave Tech is a mission-driven, customer-centric federal
+          <b>PulseWave Tech</b> is a mission-driven, customer-centric federal
           contracting company with a team that has a combined 30 years of
           military and government contracting experience. Our team is dedicated
           to exceeding customer expectations through proactive,
@@ -14,13 +14,13 @@ const MissionStatement = () => {
         </p>
         <p>
           We specialize in solving complex problems in data analysis, data
-          manipulation, artificial intelligence (AI), machine learning (ML), and
-          workflow automation. Our core expertise lies in intelligence analysis,
-          with a focus on cyber and network analysis, as well as software and
-          analytics development.
+          manipulation, artificial intelligence (AI), machine learning (ML),
+          software development, and workflow automation. Our core expertise lies
+          in intelligence analysis, with a focus on cyber and network analysis,
+          as well as software and analytics development.
         </p>
         <p>
-          At PulseWave Tech, we are committed to leveraging cutting-edge
+          At <b>PulseWave Tech</b>, we are committed to leveraging cutting-edge
           technology to deliver tailored solutions that meet the evolving needs
           of our federal clients.
         </p>
